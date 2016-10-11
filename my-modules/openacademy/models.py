f2d963aad578ceec0e36ef9c3f6767b2cdac92ae
cesargtz <cesareomargtz@gmail.com>
2016-10-11 21:12:47 +0000

    Constrains_postgres

--- my-modules/openacademy/models.py
+++ my-modules/openacademy/models.py
@@ -5,20 +5,65 @@ from openerp import models, fields, api
 class Course(models.Model): 
   _name = 'openacademy.course'
 
-  name = fields.Char(string="Title", required=True)
+  name = fields.Char(string="Titulo", required=True)
   description = fields.Text()
   responsible_id = fields.Many2one('res.users',
-				ondelete='set null', string="Responsible", index=True)
-  session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+				ondelete='set null', string="Responsable", index=True)
+  session_ids = fields.One2many('openacademy.session', 'course_id', string="Sesiones")
+
+   _sql_constraints = [
+       ('name_description_check',
+        'CHECK(name != description)',
+        "The title of the course should not be the description"),
+
+       ('name_unique',
+        'UNIQUE(name)',
+        "The course title must be unique"),
+   ]
 
 class Session(models.Model):
   _name = 'openacademy.session'
 
   name = fields.Char(required=True)
-  start_date = fields.Date()  
-  duration = fields.Float(digits=(6, 2), help="Duration in days")
-  seats = fields.Integer(string="Number of seats")
-  instructor_id = fields.Many2one('res.partner', string="Instructor")
+  start_date = fields.Date(default=fields.Date.today)
+  duration = fields.Float(digits=(6, 2), help="Duracion en dias")
+  seats = fields.Integer(string="Numero de asientos")
+  active = fields.Boolean(default=True)
+  instructor_id = fields.Many2one('res.partner', string="Instructor",
+	domain=['|', ('instructor', '=', True),
+		     ('category_id.name', 'ilike', "Maestro")])
   course_id = fields.Many2one('openacademy.course',
-				ondelete='cascade', string="Course", required=True)
-  attendee_ids = fields.Many2many('res.partner', string="Attendees")
+				ondelete='cascade', string="Curso", required=True)
+  attendee_ids = fields.Many2many('res.partner', string="Integrantes")
+  taken_seats = fields.Float(string = "Asientos ocupados", compute='_take_seats')
+
+  @api.depends('seats','attendee_ids')
+  def _take_seats(self):
+    for r in self:
+      if not r.seats:
+        r.taken_seats = 0
+      else:
+        r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+  @api.onchange('seats', 'attendee_ids')
+  def _verify_valid_seats(self):
+    if self.seats < 0:
+      return {
+        'warning' : {
+          'title' : "Incorrect 'seats' value",
+          'message' : "The number of available seats may not be negative",
+          },
+        }
+    if self.seats < len(self.attendee_ids):
+      return {
+              'warning': {
+                  'title': "Too many attendees",
+                  'message': "Increase seats or remove excess attendees",
+              },
+          } 
+
+  @api.constrains('instructor_id', 'attendee_ids')
+  def _check_instructor_not_in_attendees(self):
+    for r in self:
+      if r.instructor and r.instructor in r.attendee_ids:
+        raise exception.ValidationError("A session's instructor can't be an attendee")
