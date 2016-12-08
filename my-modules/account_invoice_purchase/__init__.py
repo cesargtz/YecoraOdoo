bc1bbf5b2e809a3071bbba61243cfee660d5ded8
cesargtz <cesareomargtz@gmail.com>
2016-12-08 16:57:25 +0000

    purchase_invoice_tons

--- my-modules/account_invoice_purchase/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-import controllers
-import account_invoice_purchase
--- my-modules/account_invoice_purchase/account_invoice_purchase.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class account_invoice_purchase(models.Model):
-#     _name = 'account_invoice_purchase.account_invoice_purchase'
-
-#     name = fields.Char()
